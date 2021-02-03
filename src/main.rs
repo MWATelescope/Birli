ebed158c8d8682baebbe0466332851a99c93d298
Derwent McElhinney <derwentx@gmail.com>
2021-02-03 17:50:42 +1100

    [EOR-6] ♻️ move main to main_with_args to test directly

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 use clap::{crate_authors, crate_description, crate_name, crate_version, App};
 use std::env;
 
-fn main() {
+fn main_with_args(args: Vec<String>) {
     App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .get_matches();
+        .get_matches_from(args);
+}
+
+fn main() {
+    main_with_args(env::args().collect())
 }
 
 #[cfg(test)]
 mod tests {
+    use super::main_with_args;
     use assert_cli;
     use std::env;
 
     #[test]
-    fn main_with_version() {
+    fn main_with_version_doesnt_crash() {
+        main_with_args(vec![String::from("--version")]);
+    }
+
+    #[test]
+    fn forked_main_with_version_prints_version() {
         let pkg_name = env!("CARGO_PKG_NAME");
         let pkg_version = env!("CARGO_PKG_VERSION");
         assert_cli::Assert::main_binary()
