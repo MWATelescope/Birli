cd70f8bd355ac99ef62971ed5698597af3a2b5f9
Derwent McElhinney <derwentx@gmail.com>
2021-03-18 15:08:58 +0800

    [EOR-11] ✅ add ord corr tests imageSet

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,7 @@ mod tests {
     use mwalib::CorrelatorContext;
 
     #[test]
-    fn test_context_to_imgset() {
+    fn test_context_to_imgset_mwax() {
         let metafits_path = "tests/data/1297526432_mwax/1297526432.metafits";
         let gpufits_paths = vec![
             "tests/data/1297526432_mwax/1297526432_20210216160014_ch117_000.fits",
@@ -70,51 +70,88 @@ mod tests {
             let aoflagger = cxx_aoflagger_new();
             let imgset = context_to_imgset(&aoflagger, &mut context, 0);
 
-            assert_eq!(
-                imgset.ImageBuffer(0).chunks(img_stride).collect::<Vec<_>>(),
-                [
-                    [
-                        0x410000 as f32,
-                        0x410100 as f32,
-                        0x410200 as f32,
-                        0x410300 as f32,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0
-                    ],
-                    [
-                        0x410008 as f32,
-                        0x410108 as f32,
-                        0x410208 as f32,
-                        0x410308 as f32,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0
-                    ],
-                    [
-                        0x410400 as f32,
-                        0x410500 as f32,
-                        0x410600 as f32,
-                        0x410700 as f32,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0
-                    ],
-                    [
-                        0x410408 as f32,
-                        0x410508 as f32,
-                        0x410608 as f32,
-                        0x410708 as f32,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0
-                    ]
-                ]
-            );
+            assert_eq!(imgset.ImageBuffer(0)[0 * img_stride + 0], 0x410000 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[0 * img_stride + 1], 0x410100 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[0 * img_stride + 2], 0x410200 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[0 * img_stride + 3], 0x410300 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[0 * img_stride + 4], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[0 * img_stride + 5], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[0 * img_stride + 6], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[0 * img_stride + 7], 0.0);
+
+            assert_eq!(imgset.ImageBuffer(0)[1 * img_stride + 0], 0x410008 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[1 * img_stride + 1], 0x410108 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[1 * img_stride + 2], 0x410208 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[1 * img_stride + 3], 0x410308 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[1 * img_stride + 4], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[1 * img_stride + 5], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[1 * img_stride + 6], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[1 * img_stride + 7], 0.0);
+
+            assert_eq!(imgset.ImageBuffer(0)[2 * img_stride + 0], 0x410400 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[2 * img_stride + 1], 0x410500 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[2 * img_stride + 2], 0x410600 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[2 * img_stride + 3], 0x410700 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[2 * img_stride + 4], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[2 * img_stride + 5], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[2 * img_stride + 6], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[2 * img_stride + 7], 0.0);
+
+            assert_eq!(imgset.ImageBuffer(0)[3 * img_stride + 0], 0x410408 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[3 * img_stride + 1], 0x410508 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[3 * img_stride + 2], 0x410608 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[3 * img_stride + 3], 0x410708 as f32);
+            assert_eq!(imgset.ImageBuffer(0)[3 * img_stride + 4], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[3 * img_stride + 5], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[3 * img_stride + 6], 0.0);
+            assert_eq!(imgset.ImageBuffer(0)[3 * img_stride + 7], 0.0);
+
+            assert_eq!(imgset.ImageBuffer(1)[0 * img_stride + 0], 0x410001 as f32);
+            assert_eq!(imgset.ImageBuffer(1)[0 * img_stride + 1], 0x410101 as f32);
+            assert_eq!(imgset.ImageBuffer(1)[0 * img_stride + 2], 0x410201 as f32);
+            assert_eq!(imgset.ImageBuffer(1)[0 * img_stride + 3], 0x410301 as f32);
+            assert_eq!(imgset.ImageBuffer(1)[0 * img_stride + 4], 0.0);
+            assert_eq!(imgset.ImageBuffer(1)[0 * img_stride + 5], 0.0);
+            assert_eq!(imgset.ImageBuffer(1)[0 * img_stride + 6], 0.0);
+            assert_eq!(imgset.ImageBuffer(1)[0 * img_stride + 7], 0.0);
+
+            /* ... */
+
+            assert_eq!(imgset.ImageBuffer(7)[0 * img_stride + 0], 0x410007 as f32);
+            assert_eq!(imgset.ImageBuffer(7)[0 * img_stride + 1], 0x410107 as f32);
+            assert_eq!(imgset.ImageBuffer(7)[0 * img_stride + 2], 0x410207 as f32);
+            assert_eq!(imgset.ImageBuffer(7)[0 * img_stride + 3], 0x410307 as f32);
+            assert_eq!(imgset.ImageBuffer(7)[0 * img_stride + 4], 0.0);
+            assert_eq!(imgset.ImageBuffer(7)[0 * img_stride + 5], 0.0);
+            assert_eq!(imgset.ImageBuffer(7)[0 * img_stride + 6], 0.0);
+            assert_eq!(imgset.ImageBuffer(7)[0 * img_stride + 7], 0.0);
+        }
+    }
+
+    #[test]
+    fn test_context_to_imgset_mwa_ord() {
+        let metafits_path = "tests/data/1196175296_mwa_ord/1196175296.metafits";
+        let gpufits_paths = vec![
+            "tests/data/1196175296_mwa_ord/1196175296_20171201145440_gpubox01_00.fits",
+            "tests/data/1196175296_mwa_ord/1196175296_20171201145540_gpubox01_01.fits",
+            "tests/data/1196175296_mwa_ord/1196175296_20171201145440_gpubox02_00.fits",
+            "tests/data/1196175296_mwa_ord/1196175296_20171201145540_gpubox02_01.fits",
+        ];
+        let mut context = CorrelatorContext::new(&metafits_path, &gpufits_paths).unwrap();
+
+        unsafe {
+            let aoflagger = cxx_aoflagger_new();
+            let imgset = context_to_imgset(&aoflagger, &mut context, 0);
+
+            assert_eq!(imgset.ImageBuffer(0)[0], 0x10c5be as f32);
+            assert_eq!(imgset.ImageBuffer(0)[1], 0x14c5be as f32);
+            assert_eq!(imgset.ImageBuffer(0)[2], 0x18c5be as f32);
+            assert_eq!(imgset.ImageBuffer(0)[3], 0x1cc5be as f32);
+
+            assert_eq!(imgset.ImageBuffer(1)[0], 0x10c5bf as f32);
+            assert_eq!(imgset.ImageBuffer(1)[1], 0x14c5bf as f32);
+            assert_eq!(imgset.ImageBuffer(1)[2], 0x18c5bf as f32);
+            assert_eq!(imgset.ImageBuffer(1)[3], 0x1cc5bf as f32);
         }
     }
 }
