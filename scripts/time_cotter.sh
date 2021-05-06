6a8abc7396fb15ade6974d513248bbd54d7b5169
Derwent McElhinney <derwentx@gmail.com>
2021-05-06 16:19:12 +0800

    [EOR-20] ⏱ Update benchmark results

---
+++ scripts/time_cotter.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -ex
+shopt -s globstar
+shopt -s extglob
+
+if [[ -z "$BIRLI_TEST_DIR" ]]; then
+    echo "env var \$BIRLI_TEST_DIR not set."
+    exit 1
+fi
+
+export FLAGS_OUT_DIR="$BIRLI_TEST_DIR/1247842824_vis/flags_cotter"
+mkdir -p "$FLAGS_OUT_DIR"
+
+for i in $(seq 10); do
+    if [ -n "$(ls "$BIRLI_TEST_DIR/*")" ]; then
+        rm "$BIRLI_TEST_DIR/*"
+    fi
+    time cotter \
+        -m "$BIRLI_TEST_DIR/1247842824_vis/1247842824.metafits" \
+        -o "$FLAGS_OUT_DIR/Flagfile_Cotter_%%.mwaf" \
+        -nostats \
+        -nogeom \
+        -noantennapruning \
+        -nosbgains \
+        -noflagautos \
+        -noflagdcchannels \
+        -nocablelength \
+        -edgewidth 0 \
+        -initflag 0 \
+        -endflag 0 \
+        -flag-strategy "/usr/local/share/aoflagger/strategies/mwa-default.lua" \
+        $BIRLI_TEST_DIR/1247842824_vis/1247842824*gpubox*_00.fits |
+        tee "output-$i.log"
+done
