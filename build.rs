02ce6e198ded868a4422f41f17a6385cdef27f78
Derwent McElhinney <derwentx@gmail.com>
2021-05-13 16:00:33 +0800

    🐛 fix docs.rs build

--- build.rs
+++ build.rs
@@ -1,20 +1,24 @@
 extern crate cxx_build;
+use std::env;
 
 fn main() {
     println!("cargo:rustc-cfg=verbose");
 
-    cxx_build::bridge("src/cxx_aoflagger.rs")
-        .flag_if_supported("-std=c++11")
-        .flag_if_supported("-Wno-nonportable-include-path")
-        .flag_if_supported("-Wno-unused-parameter")
-        // .include("include/cxx_aoflagger.h")
-        .file("src/cxx_aoflagger.cc")
-        .compile("cxx_aoflagger");
+    match env::var("DOCS_RS").as_deref() {
+        Ok("1") => (),
+        _ => {
+            cxx_build::bridge("src/cxx_aoflagger.rs")
+                .flag_if_supported("-std=c++11")
+                .flag_if_supported("-Wno-nonportable-include-path")
+                .flag_if_supported("-Wno-unused-parameter")
+                .file("src/cxx_aoflagger.cc")
+                .compile("cxx_aoflagger");
+        }
+    }
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/cxx_aoflagger.rs");
     println!("cargo:rerun-if-changed=src/cxx_aoflagger.cc");
-    println!("cargo:rerun-if-changed=include/cxx_aoflagger.h");
 
     // Tell cargo to tell rustc to link the aoflagger shared library.
     println!("cargo:rustc-link-lib=dylib=aoflagger");
