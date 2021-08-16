a3631a08c695e4a52079cfb29cfed488fc06e23b
Derwent McElhinney <derwentx@gmail.com>
2021-08-16 09:02:50 +0000

    ⌚ add benchmark for uvfits output

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,9 +1,13 @@
-use birli::{context_to_baseline_imgsets, correct_cable_lengths, cxx_aoflagger_new};
+use birli::{
+    context_to_baseline_imgsets, correct_cable_lengths, correct_geometry, cxx_aoflagger_new,
+    init_baseline_flagmasks, io::write_uvfits,
+};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use glob::glob;
 use mwalib::CorrelatorContext;
 use std::env;
 use std::path::Path;
+use tempfile::tempdir;
 
 fn get_context_mwax_half_1247842824() -> CorrelatorContext {
     let test_dir = env::var("BIRLI_TEST_DIR").unwrap();
@@ -38,6 +42,28 @@ fn get_context_ord_half_1196175296() -> CorrelatorContext {
         .to_owned();
     let gpufits_glob = vis_path
         .join("1196175296_*gpubox*_00.fits")
+        .to_str()
+        .unwrap()
+        .to_owned();
+    let gpufits_files: Vec<String> = glob(gpufits_glob.as_str())
+        .unwrap()
+        .filter_map(Result::ok)
+        .map(|path| path.to_str().unwrap().to_owned())
+        .collect();
+    CorrelatorContext::new(&metafits_path, &gpufits_files).unwrap()
+}
+
+fn get_context_1254670392_avg() -> CorrelatorContext {
+    let test_dir = env::var("BIRLI_TEST_DIR").unwrap();
+    let test_path = Path::new(&test_dir);
+    let vis_path = test_path.join("1254670392_vis");
+    let metafits_path = vis_path
+        .join("1254670392.metafits")
+        .to_str()
+        .unwrap()
+        .to_owned();
+    let gpufits_glob = vis_path
+        .join("1254670392_*gpubox*_00.fits")
         .to_str()
         .unwrap()
         .to_owned();
@@ -128,6 +154,101 @@ fn bench_correct_cable_lengths_ord_half_1196175296(crt: &mut Criterion) {
     });
 }
 
+fn bench_correct_geometry_mwax_half_1247842824(crt: &mut Criterion) {
+    let aoflagger = unsafe { cxx_aoflagger_new() };
+    let context = get_context_mwax_half_1247842824();
+    let img_coarse_chan_idxs = &context.common_coarse_chan_indices;
+    let img_timestep_idxs = &context.common_timestep_indices;
+    let baseline_idxs = (0..context.metafits_context.num_baselines).collect::<Vec<_>>();
+    let mut baseline_imgsets = context_to_baseline_imgsets(
+        &aoflagger,
+        &context,
+        img_coarse_chan_idxs,
+        &context.common_timestep_indices.clone(),
+        None,
+    );
+    crt.bench_function("correct_geometry - mwax_half_1247842824", |bch| {
+        bch.iter(|| {
+            correct_geometry(
+                black_box(&context),
+                black_box(&baseline_idxs),
+                black_box(&mut baseline_imgsets),
+                black_box(img_coarse_chan_idxs),
+                black_box(img_timestep_idxs),
+                None,
+            );
+        })
+    });
+}
+
+fn bench_correct_geometry_ord_half_1196175296(crt: &mut Criterion) {
+    let aoflagger = unsafe { cxx_aoflagger_new() };
+    let context = get_context_ord_half_1196175296();
+    let img_coarse_chan_idxs = &context.common_coarse_chan_indices;
+    let img_timestep_idxs = &context.common_timestep_indices;
+    let baseline_idxs = (0..context.metafits_context.num_baselines).collect::<Vec<_>>();
+    let mut baseline_imgsets = context_to_baseline_imgsets(
+        &aoflagger,
+        &context,
+        img_coarse_chan_idxs,
+        &context.common_timestep_indices.clone(),
+        None,
+    );
+    crt.bench_function("correct_geometry - ord_half_1196175296", |bch| {
+        bch.iter(|| {
+            correct_geometry(
+                black_box(&context),
+                black_box(&baseline_idxs),
+                black_box(&mut baseline_imgsets),
+                black_box(img_coarse_chan_idxs),
+                black_box(img_timestep_idxs),
+                None,
+            );
+        })
+    });
+}
+
+fn bench_uvfits_output_1254670392_avg_none(crt: &mut Criterion) {
+    let aoflagger = unsafe { cxx_aoflagger_new() };
+    let context = get_context_1254670392_avg();
+    let img_coarse_chan_idxs = &context.common_coarse_chan_indices;
+    let img_timestep_idxs = &context.common_timestep_indices;
+    let baseline_idxs = (0..context.metafits_context.num_baselines).collect::<Vec<_>>();
+    let baseline_imgsets = context_to_baseline_imgsets(
+        &aoflagger,
+        &context,
+        img_coarse_chan_idxs,
+        &context.common_timestep_indices.clone(),
+        None,
+    );
+
+    let tmp_dir = tempdir().unwrap();
+    let uvfits_path = tmp_dir.path().join("1254670392.none.uvfits");
+
+    let baseline_flagmasks = init_baseline_flagmasks(
+        &aoflagger,
+        &context,
+        img_coarse_chan_idxs,
+        &img_timestep_idxs,
+        None,
+    );
+
+    crt.bench_function("correct_geometry - ord_half_1196175296", |bch| {
+        bch.iter(|| {
+            write_uvfits(
+                black_box(uvfits_path.as_path()),
+                black_box(&context),
+                black_box(&baseline_idxs),
+                black_box(&baseline_imgsets),
+                black_box(&baseline_flagmasks),
+                black_box(&img_timestep_idxs),
+                black_box(img_coarse_chan_idxs),
+                None,
+            ).unwrap();
+        });
+    });
+}
+
 criterion_group!(
     name = benches;
     config = Criterion::default().sample_size(10);
@@ -136,5 +257,8 @@ criterion_group!(
         bench_context_to_baseline_imgsets_ord_half_1196175296,
         bench_correct_cable_lengths_mwax_half_1247842824,
         bench_correct_cable_lengths_ord_half_1196175296,
+        bench_correct_geometry_mwax_half_1247842824,
+        bench_correct_geometry_ord_half_1196175296,
+        bench_uvfits_output_1254670392_avg_none,
 );
 criterion_main!(benches);
